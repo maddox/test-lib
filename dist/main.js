53e7a8b926be8bccb227060fd1708a77bd0912b0
probablycorey <probablycorey@github.com>
2018-02-27 12:47:00 -0800

    Include greatness

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      _export("default", (numberA, numberB) => {
+        return numberA + numberB;
+      });
+    }
+  };
+});
+
---
+++ main.js
@@ -0,0 +1,3 @@
+export default (numberA, numberB) => {
+  return numberA + numberB
+}
